64fc6754edc51bca360a68953e91a5d050d8e3d1
yomce <kky032911@gmail.com>
2025-04-26 16:21:23 +0900

    extend_heap

--- malloc-lab/mm.c
+++ malloc-lab/mm.c
@@ -106,6 +106,25 @@ void *mm_malloc(size_t size)
     }
 }
 
+static void *extend_heap(size_t words)
+{
+    char *bp;
+    size_t size;
+
+    /* Allocate an even number of words to maintain alignment */
+    size = (words % 2) ? (words+1) * WSIZE : words * WSIZE;
+    if ((long)(bp = mem_sbrk(size)) == -1)
+        return NULL;
+
+    /* Initialize free block header/footer and the epilogue header */
+    PUT(HDRP(bp), PACK(size, 0)); /* Free block header */
+    PUT(FTRP(bp), PACK(size, 0)); /* Free block footer */
+    PUT(HDRP(NEXT_BLKP(bp)), PACK(0, 1)); /* New epilogue header */
+
+    /* Coalesce if the previous block was free */
+    return coalesce(bp);
+}
+
 /*
  * mm_free - Freeing a block does nothing.
  */
