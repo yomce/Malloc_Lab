3bc9321662167b04ad3c064b84de0532a080ec99
yomce <kky032911@gmail.com>
2025-04-26 17:02:34 +0900

    mm_free

--- malloc-lab/mm.c
+++ malloc-lab/mm.c
@@ -128,8 +128,46 @@ static void *extend_heap(size_t words)
 /*
  * mm_free - Freeing a block does nothing.
  */
-void mm_free(void *ptr)
+void mm_free(void *bp)
 {
+size_t size = GET_SIZE(HDRP(bp));
+
+PUT(HDRP(bp), PACK(size, 0));
+PUT(FTRP(bp), PACK(size, 0));
+coalesce(bp);
+}
+
+static void *coalesce(void *bp)
+{
+size_t prev_alloc = GET_ALLOC(FTRP(PREV_BLKP(bp)));
+size_t next_alloc = GET_ALLOC(HDRP(NEXT_BLKP(bp)));
+size_t size = GET_SIZE(HDRP(bp));
+
+if (prev_alloc && next_alloc) { /* Case 1 */
+return bp;
+}
+
+else if (prev_alloc && !next_alloc) { /* Case 2 */
+size += GET_SIZE(HDRP(NEXT_BLKP(bp)));
+PUT(HDRP(bp), PACK(size, 0));
+PUT(FTRP(bp), PACK(size,0));
+}
+
+else if (!prev_alloc && next_alloc) { /* Case 3 */
+size += GET_SIZE(HDRP(PREV_BLKP(bp)));
+PUT(FTRP(bp), PACK(size, 0));
+PUT(HDRP(PREV_BLKP(bp)), PACK(size, 0));
+bp = PREV_BLKP(bp);
+}
+
+else { /* Case 4 */
+size += GET_SIZE(HDRP(PREV_BLKP(bp))) +
+GET_SIZE(FTRP(NEXT_BLKP(bp)));
+PUT(HDRP(PREV_BLKP(bp)), PACK(size, 0));
+PUT(FTRP(NEXT_BLKP(bp)), PACK(size, 0));
+bp = PREV_BLKP(bp);
+}
+return bp;
 }
 
 /*
